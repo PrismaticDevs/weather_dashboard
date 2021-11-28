2df59d366599cccb40d89cab1003496da5004037
Matthew Brignola <matthewbrignola@ymail.com>
2021-11-28 10:43:54 -0700

    removed extra code

--- js/script.js
+++ js/script.js
@@ -49,14 +49,6 @@ makeHistoryButtons();
 // Fetch Async Await
 async function weatherData() {
     let d = new Date;
-    const weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
     let month = d.getMonth() + 1;
     let x = d.getDate();
     let year = d.getFullYear();
